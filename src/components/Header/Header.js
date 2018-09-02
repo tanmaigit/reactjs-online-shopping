dbd0945e9cd383b37de63c763f9d1afbbcd22b32
Tan Mai <tanmai@kms-technology.com>
2018-09-02 12:36:32 +0700

    add slider, banner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import TopHeader from './TopHeader';
+import Slider from './Slider';
+import Banner from './Banner';
 
-const Header = () => <TopHeader />;
+const Header = () => (
+	<div id="header">
+		<TopHeader />
+		<Slider />
+		<Banner />
+	</div>
+);
+
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './Style.css';
+import './css/Style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
