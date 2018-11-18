ec4dcd51b18ef151eb0a88b555f5cd55ecac3b4b
Tan Mai <tanmai@kms-technology.com>
2018-11-18 11:31:36 +0700

    add umd popperjs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
-import 'popper.js/dist/popper.min.js';
+import 'popper.js/dist/umd/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import App from './App';
